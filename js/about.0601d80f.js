dadd8e7d46a08a1f87dc3d851443fab6949f2c55
alikurtulush92 <kopanarya92@gmail.com>
2020-08-11 15:59:40 +0100

    deploy

---
+++ js/about.0601d80f.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,a){"use strict";a.r(e);var n=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,a=t._self._c||e;return a("div",{staticClass:"about"},[a("h1",[t._v("This is an about page")]),a("p",[t._v("This is a todolist app.")])])}],i=a("2877"),u={},l=Object(i["a"])(u,n,s,!1,null,null,null);e["default"]=l.exports}}]);
+//# sourceMappingURL=about.0601d80f.js.map

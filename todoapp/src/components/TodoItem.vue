2d73a98039e7094f44a7115a1f320d48269db0e5
alikurtulush92 <kopanarya92@gmail.com>
2020-11-05 09:04:01 +0000

    delete spaces and check indentation

--- todoapp/src/components/TodoItem.vue
+++ todoapp/src/components/TodoItem.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
         <p>
-            <input type="checkbox" v-on:change="markComplete" >
-            {{todo.title}}
-            <button @click="$emit('del-todo',todo.id)" class="del">x</button>
-            </p>
+        <input type="checkbox" v-on:change="markComplete" >
+        {{todo.title}}
+        <button @click="$emit('del-todo',todo.id)" class="del">x</button>
+        </p>
     </div>
 </template>
 <script>
@@ -19,21 +19,21 @@ export default {
 }
 </script>
 <style  scoped>
- .todo-item{
-     background: #f4f4f4;
-     padding: 10px;
-     border-bottom: 1px #ccc dotted;
-     }
- .is-complete{
-         text-decoration: line-through;
-     }
- .del {
-     background: #ff0000;
-     color:#fff;
-     border:none;
-     padding:  5px 9px;
-     border-radius:   50%;
-     cursor: pointer;
-     float: right;
- }   
+.todo-item{
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+}
+.is-complete{
+    text-decoration: line-through;
+}
+.del {
+    background: #ff0000;
+    color:#fff;
+    border:none;
+    padding:  5px 9px;
+    border-radius:   50%;
+    cursor: pointer;
+    float: right;
+}   
 </style>
